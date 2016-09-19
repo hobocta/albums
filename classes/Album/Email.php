d51d513454d3b9eb6df971ba37f049e5f264eeb5
Антон Кондратов <contact@site-home.ru>
2016-09-19 12:49:00 +0300

    Добавил ссылку на поиск в гугле

--- classes/Album/Email.php
+++ classes/Album/Email.php
@@ -5,16 +5,19 @@ final class Email
 {
 	public static function send($email, array $artist, array $album)
 	{
-		$headers = sprintf('From: %s', $email);
+		$searchString = urlencode(sprintf('%s %s', $artist['name'], $album['name']));
 
-		return mail(
-			$email,
-			sprintf('Новый альбом: %s - %s', $artist['name'], $album['name']),
-			sprintf(
-				'https://rutracker.org/forum/tracker.php?nm=%s',
-				urlencode(sprintf('%s %s', $artist['name'], $album['name']))
-			),
-			$headers
+		$links = array(
+			sprintf('https://rutracker.org/forum/tracker.php?nm=%s', $searchString),
+			sprintf('https://www.google.ru/#newwindow=1&q=%s', $searchString),
 		);
+
+		$mail = array(
+			'headers' => sprintf('From: %s', $email),
+			'title' => sprintf('Новый альбом: %s - %s', $artist['name'], $album['name']),
+			'body' => implode(PHP_EOL, $links),
+		);
+
+		return mail($email, $mail['title'], $mail['body'], $mail['headers']);
 	}
 }
